6acc7b048dfb87c64e7d0758853d293cd70b4597
vitorhugogomes <vitorhugogomesbarbosa09@gmail.com>
2025-04-17 09:53:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function enterDictPW() {
+    window.location.href = "dict/pw.html"
+}
+
+function enterDictFI() {
+    window.location.href = "dict/fi.html"
+}
+
+function enterDictDD() {
+    window.location.href = "dict/dd.html"
+}
+
+function enterDictTPA() {
+    window.location.href = "dict/tpa.html"
+}
