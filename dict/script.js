c54118f5a43b36365f5c1f5b0cda0df5a3188569
vitorhugogomes <vitorhugogomesbarbosa09@gmail.com>
2025-04-17 09:54:54 -0300

    Add files via upload

---
+++ dict/script.js
@@ -0,0 +1,19 @@
+function enterHome() {
+    window.location.href = "../index.html"
+}
+
+function enterDictPW() {
+    window.location.href = "pw.html"
+}
+
+function enterDictFI() {
+    window.location.href = "fi.html"
+}
+
+function enterDictDD() {
+    window.location.href = "dd.html"
+}
+
+function enterDictTPA() {
+    window.location.href = "tpa.html"
+}
